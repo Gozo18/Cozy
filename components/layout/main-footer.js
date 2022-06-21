bfb260cf7cefcf0a9c3b847addab59ed9673d81c
Roman Jelínek <jelinek82@gmail.com>
2022-06-21 16:56:28 +0200

    Auth removed

--- components/layout/main-footer.js
+++ components/layout/main-footer.js
@@ -1,60 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useContext } from "react";
-import AuthContext from "@/context/AuthContext";
 
 import styles from "@/styles/Footer.module.scss";
 
 function MainFooter() {
-  const { user, logout } = useContext(AuthContext);
-
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        {/* <Link href='/'>
-          <a>
-            <Image
-              src='/CB.svg'
-              alt='Cozy Buddy Logo'
-              width={300}
-              height={50}
-            />
-          </a>
-        </Link> */}
-
-        {user ? (
-          <>
-            {/* If logged in */}
-            <Link href='/nase-prace/nova'>
-              <a className={styles.addLink}>Přidat novou práci</a>
-            </Link>
-            <Link href='/account/dashboard'>
-              <a className={styles.addLink}>Dashboard</a>
-            </Link>
-            <a
-              href='#'
-              onClick={(e) => {
-                e.preventDefault();
-                logout();
-              }}
-              className={styles.addLink}
-            >
-              Odhlásit
-            </a>
-            <p>© 2022 cozy buddy</p>
-          </>
-        ) : (
-          <div className={styles.notLogged}>
-            <p>© 2022 cozy buddy</p>
-            {/* If logged out */}
-            {/* <Link href='/account/login'>
-              <a>Přihlásit se</a>
-            </Link> */}
-            {/* <Link href='/account/register'>
-              <a className={styles.addLink}>Registrovat se</a>
-            </Link> */}
-          </div>
-        )}
+        <div className={styles.notLogged}>
+          <p>© 2022 cozy buddy</p>
+        </div>
       </div>
     </footer>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,34 @@
 import Head from "next/head";
 import Layout from "@/components/layout/layout";
-import { AuthProvider } from "@/context/AuthContext";
 
 import "@/styles/globals.scss";
 import "lightgallery/scss/lightgallery.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Layout>
-        <Head>
-          <title>Cozy Buddy</title>
-          <meta
-            name='description'
-            content='Cozy Buddy - čalounictví a nábytek. Zakázková výroba, oprava nábytku a automotive potahů.'
-          />
-          <meta httpEquiv='content-language' content='cs' />
-          <meta name='robots' content='index,follow' />
-          <link rel='icon' href='/favicon.ico' />
-          <link rel='preconnect' href='https://fonts.googleapis.com'></link>
-          <link
-            rel='preconnect'
-            href='https://fonts.gstatic.com'
-            crossOrigin
-          ></link>
-          <link
-            href='https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&display=swap'
-            rel='stylesheet'
-          ></link>
-        </Head>
-        <Component {...pageProps} />
-      </Layout>
-    </AuthProvider>
+    <Layout>
+      <Head>
+        <title>Cozy Buddy</title>
+        <meta
+          name='description'
+          content='Cozy Buddy - čalounictví a nábytek. Zakázková výroba, oprava nábytku a automotive potahů.'
+        />
+        <meta httpEquiv='content-language' content='cs' />
+        <meta name='robots' content='index,follow' />
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://fonts.googleapis.com'></link>
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin
+        ></link>
+        <link
+          href='https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&display=swap'
+          rel='stylesheet'
+        ></link>
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
